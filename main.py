37b3da74d75b37e2c4793f9bb20fef0c515b9c02
su1jun <qaxsd530@gmail.com>
2025-06-15 17:19:39 +0900

    feat(request body): 요청 본문 사용
    - basemodel을 통해 커스텀 데이터 모델을 선언한 후
      해당 모델을 파라미터/본문 내용 으로써 활용

--- main.py
+++ main.py
@@ -1,79 +1,43 @@
-from enum import Enum
-from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+# Pydantic datamodel
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 app = FastAPI()
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-""" @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {"item_id": item_id} """
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name is ModelName.alexnet: # model_name == ModelName.alexnet과 동일
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-    
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-    
-    return {"model_name": model_name, "message": "Have some residuals"}
-
-# query parameter
-fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-@app.get("/items/")
-async def read_item(skip: int = 0, limit: int = 10):
-    return fake_items_db[skip : skip + limit]
-
-# Union[str, None] = None 은 두 가지 타입 중 하나를 받을 수 있음 (str 또는 None)
-""" @app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id} """
-
-# 쿼리 매개변수 형변환
-@app.get("/items/{item_id}")
-async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+""" @app.post("/items/")
+async def create_item(item: Item):
     return item
 
-# multiple route/query parameter
-@app.get("/users/{user_id}/items/{item_id}")
-async def read_user_item(user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False):
-    item = {"item_id": item_id, "owner_id": user_id}
-    if q:
-        item.update({"q": q})
-    if not short:
-        item.update(
-            {"description": "This is an amazing item that has a long description"}
-        )
+# parameter description
+@app.post("/items/")
+async def create_item(item: Item):
     return item
+ """
+# use pydantic model to modify the request body
+## request body
+@app.post("/items/")
+async def create_item(item: Item):
+    item_dict = item.dict()
+    if item.tax is not None:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
 
-# required query parameter
-@app.get("/items/{item_id}")
-async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None):
-    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
-    return item
+## request body, route parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
+
+## request body, route parameters, query parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+    if q:
+        result.update({"q": q})
+    return result
