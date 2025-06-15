75e2d855fa842d0c52f9b269d3cbcb781ab491e2
su1jun <qaxsd530@gmail.com>
2025-06-15 17:05:12 +0900

    feat(parameter) : 경로 파라미터, 매개변수 핸들링
    - enum을 통한 커스텀 데이터 구조 선언, 경로 파라미터에 사용 (단, 추천은 안함)
    - 고정링크/변동링크 처리시 순서를 신경써야함
    - 파라미터의 구분
    - 파라미터 형변환, 값 업
    - 기본값의 존재에 따라 파라미터 필수 여부 선택
    - 여러 종류의 파라미터를 경로에 포함할 수 있음

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Union
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -11,9 +12,9 @@ class ModelName(str, Enum):
 async def root():
     return {"message": "Hello World"}
 
-@app.get("/items/{item_id}")
+""" @app.get("/items/{item_id}")
 async def read_item(item_id: int):
-    return {"item_id": item_id}
+    return {"item_id": item_id} """
 
 @app.get("/users/me")
 async def read_user_me():
@@ -25,10 +26,54 @@ async def read_user(user_id: str):
 
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
+    if model_name is ModelName.alexnet: # model_name == ModelName.alexnet과 동일
         return {"model_name": model_name, "message": "Deep Learning FTW!"}
     
     if model_name.value == "lenet":
         return {"model_name": model_name, "message": "LeCNN all the images"}
     
     return {"model_name": model_name, "message": "Have some residuals"}
+
+# query parameter
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
+
+# Union[str, None] = None 은 두 가지 타입 중 하나를 받을 수 있음 (str 또는 None)
+""" @app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None):
+    if q:
+        return {"item_id": item_id, "q": q}
+    return {"item_id": item_id} """
+
+# 쿼리 매개변수 형변환
+@app.get("/items/{item_id}")
+async def read_item(item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# multiple route/query parameter
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: int, item_id: str, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+# required query parameter
+@app.get("/items/{item_id}")
+async def read_user_item(item_id: str, needy: str, skip: int = 0, limit: Union[int, None] = None):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
