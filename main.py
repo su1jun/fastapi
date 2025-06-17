f65578a7981403c5311423a3992953c4d6b1b08c
su1jun <qaxsd530@gmail.com>
2025-06-17 23:45:56 +0900

    feat(vaild) : 필수 변수값 적용

--- main.py
+++ main.py
@@ -11,12 +11,28 @@ async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max
     return results """
 
 # add regex pattern
-@app.get("/items/")
+""" @app.get("/items/")
 async def read_items(
     q: Union[str, None] = Query(
         default=None, min_length=3, max_length=50, pattern="^fixedquery$"
     ),
 ):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# default value
+@app.get("/items/")
+async def read_items(q: str = Query(default="fixedquery", min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+# required query parameter
+@app.get("/items/")
+async def read_items(q: str = Query(min_length=3)): # from typing_extensions import Annotated; (q: Annotated[str, Query(min_length=3)])
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
