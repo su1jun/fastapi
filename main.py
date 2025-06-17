337008e02f326f0dc75fdea7c9267d6684617784
su1jun <qaxsd530@gmail.com>
2025-06-17 23:40:20 +0900

    feat(vaild) : 정규식을 활용한 변수값 검증

--- main.py
+++ main.py
@@ -3,8 +3,20 @@ from fastapi import FastAPI, Query
 
 app = FastAPI()
 
-@app.get("/items/")
+""" @app.get("/items/")
 async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# add regex pattern
+@app.get("/items/")
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None, min_length=3, max_length=50, pattern="^fixedquery$"
+    ),
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
