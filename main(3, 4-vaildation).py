e5ea5bddea596c37e074a4b0539ee55fcb1c377a
su1jun <qaxsd530@gmail.com>
2025-06-19 19:44:00 +0900

    feat(query-model) : pydantic 사용
    - 한 모델을 여러 곳에서 재사용 가능
    - 파라미터에 대한 검증 및 메타데이터를 한 번에 선언

---
+++ main(3, 4-vaildation).py
@@ -0,0 +1,151 @@
+from typing import Union, List
+from fastapi import FastAPI, Query, Path
+
+app = FastAPI()
+
+""" @app.get("/items/")
+async def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=50)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# add regex pattern
+""" @app.get("/items/")
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None, min_length=3, max_length=50, pattern="^fixedquery$"
+    ),
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# default value
+""" @app.get("/items/")
+async def read_items(q: str = Query(default="fixedquery", min_length=3)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# required query parameter
+""" @app.get("/items/")
+async def read_items(q: str = Query(min_length=3)): # from typing_extensions import Annotated; (q: Annotated[str, Query(min_length=3)])
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# multiple query parameters
+@app.get("/items/")
+async def read_items(q: Union[List[str], None] = Query(default=None)): # (q: list = Query(default=[]))
+    query_items = {"q": q}
+    return query_items
+
+# add metadata to query parameter
+""" @app.get("/items/")
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+    ),
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# add alias to query parameter
+""" @app.get("/items/")
+async def read_items(q: Union[str, None] = Query(default=None, alias="item-query")): # http://127.0.0.1:8000/items/?item-query=foobaritems <- alias
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# add deprecated to query parameter
+""" @app.get("/items/")
+async def read_items(
+    q: Union[str, None] = Query(
+        default=None,
+        alias="item-query",
+        title="Query string",
+        description="Query string for the items to search in the database that have a good match",
+        min_length=3,
+        max_length=50,
+        pattern="^fixedquery$",
+        deprecated=True,
+    ),
+):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# add metadata to path parameter
+""" @app.get("/items/{item_id}")
+async def read_items(
+    item_id: int = Path(title="The ID of the item to get"),
+    q: Union[str, None] = Query(default=None, alias="item-query"),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# consider order of parameters <- not required in fastapi
+""" @app.get("/items/{item_id}")
+async def read_items(q: str, item_id: int = Path(title="The ID of the item to get")):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# use * -> path parameter must be before query parameter
+""" @app.get("/items/{item_id}")
+async def read_items(*, item_id: int = Path(title="The ID of the item to get"), q: str):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results """
+
+# add ge to path parameter
+@app.get("/items/{item_id}")
+async def read_items(
+    *, item_id: int = Path(title="The ID of the item to get", ge=1), q: str
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+# add gt, le to path parameter
+@app.get("/items/{item_id}")
+async def read_items(
+    *,
+    item_id: int = Path(title="The ID of the item to get", gt=0, le=1000),
+    q: str,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
+
+# add gt, ge, lt, le to query parameter
+@app.get("/items/{item_id}")
+async def read_items(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: str,
+    size: float = Query(gt=0, lt=10.5),
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if size:
+        results.update({"size": size})
+    return results
