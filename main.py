9f85d4c0f1712e638df94c066c99b724d617dd44
su1jun <qaxsd530@gmail.com>
2025-06-19 19:47:08 +0900

    feat(query-model) : 매개변수 제한 조치

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel, Field
 app = FastAPI()
 
 class FilterParams(BaseModel):
+    # model_config = {"extra": "forbid"} # extra parameters are not allowed
+
     limit: int = Field(100, gt=0, le=100)
     offset: int = Field(0, ge=0)
     order_by: Literal["created_at", "updated_at"] = "created_at"
