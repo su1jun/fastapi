680242751f29962652bf1787eb8d586c685d5bce
su1jun <qaxsd530@gmail.com>
2025-06-20 22:25:43 +0900

    feat(body) : 다중 본문 매개변수와 쿼리
    - body 모듈을 사용하면 본문 내 특정 키에 직접 주입이 가능,
     = 쿼리파라미터로 받지 않아도 됨
    
    - fastapi가 파라미터 종류를 알아서 구별해줌

--- main.py
+++ main.py
@@ -38,6 +38,16 @@ async def update_item(item_id: int, item: Item, user: User):
 # third
 # add Body
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+# async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+async def update_item(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: int = Body(gt=0),
+    q: Union[str, None] = None,
+):
     results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    if q:
+        results.update({"q": q})
     return results
