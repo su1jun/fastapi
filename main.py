3e4e9c8d45ce8565df70dae53cf9ca4e939b65d9
su1jun <qaxsd530@gmail.com>
2025-06-20 22:16:27 +0900

    feat(body) : 본문 매개변수 다중 사용

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ class Item(BaseModel):
     price: float
     tax: Union[float, None] = None
 
-@app.put("/items/{item_id}")
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+
+""" @app.put("/items/{item_id}")
 async def update_item(
     *,
     item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
@@ -21,4 +26,9 @@ async def update_item(
         results.update({"q": q})
     if item:
         results.update({"item": item})
+    return results """
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
     return results
