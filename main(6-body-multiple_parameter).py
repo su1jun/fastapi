fd458039bc8a428b64c1de1e1e6403d1b7f43e84
su1jun <qaxsd530@gmail.com>
2025-06-22 23:41:54 +0900

    refactor : 폴더 구조 변경

---
+++ main(6-body-multiple_parameter).py
@@ -0,0 +1,59 @@
+from typing import Union
+from fastapi import FastAPI, Path, Body
+from pydantic import BaseModel
+
+app = FastAPI()
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+class User(BaseModel):
+    username: str
+    full_name: Union[str, None] = None
+
+# frist
+""" @app.put("/items/{item_id}")
+async def update_item(
+    *,
+    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
+    q: Union[str, None] = None,
+    item: Union[Item, None] = None,
+):
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    if item:
+        results.update({"item": item})
+    return results """
+
+# second
+# use multiple body parameters
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
+
+# third
+# add Body
+@app.put("/items/{item_id}")
+# async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
+async def update_item(
+    *,
+    item_id: int,
+    item: Item,
+    user: User,
+    importance: int = Body(gt=0),
+    q: Union[str, None] = None,
+):
+    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
+    if q:
+        results.update({"q": q})
+    return results
+
+# fourth
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item = Body(embed=True)):
+    results = {"item_id": item_id, "item": item}
+    return results
--- main.py
+++ main.py
@@ -1,61 +1,21 @@
-from typing import Union
-from fastapi import FastAPI, Path, Body
-from pydantic import BaseModel
+from typing import Annotated
+
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
+
+
 class Item(BaseModel):
     name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
+    description: str | None = Field(
+        default=None, title="The description of the item", max_length=300
+    )
+    price: float = Field(gt=0, description="The price must be greater than zero")
+    tax: float | None = None
 
-class User(BaseModel):
-    username: str
-    full_name: Union[str, None] = None
 
-# frist
-""" @app.put("/items/{item_id}")
-async def update_item(
-    *,
-    item_id: int = Path(title="The ID of the item to get", ge=0, le=1000),
-    q: Union[str, None] = None,
-    item: Union[Item, None] = None,
-):
-    results = {"item_id": item_id}
-    if q:
-        results.update({"q": q})
-    if item:
-        results.update({"item": item})
-    return results """
-
-# second
-# use multiple body parameters
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item, user: User):
-    results = {"item_id": item_id, "item": item, "user": user}
-    return results
-
-# third
-# add Body
-@app.put("/items/{item_id}")
-# async def update_item(item_id: int, item: Item, user: User, importance: int = Body()):
-async def update_item(
-    *,
-    item_id: int,
-    item: Item,
-    user: User,
-    importance: int = Body(gt=0),
-    q: Union[str, None] = None,
-):
-    results = {"item_id": item_id, "item": item, "user": user, "importance": importance}
-    if q:
-        results.update({"q": q})
-    return results
-
-# fourth
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item = Body(embed=True)):
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     results = {"item_id": item_id, "item": item}
     return results
-
-##
