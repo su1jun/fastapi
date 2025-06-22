3450b8b6918727484e5277280e1cae000b4e25dd
su1jun <qaxsd530@gmail.com>
2025-06-22 23:46:40 +0900

    feat : recursive model
    - 모델의 속성을 항목 리스트로 사용

--- main.py
+++ main.py
@@ -1,21 +1,17 @@
-from typing import Annotated
-
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-
 class Item(BaseModel):
     name: str
-    description: str | None = Field(
-        default=None, title="The description of the item", max_length=300
-    )
-    price: float = Field(gt=0, description="The price must be greater than zero")
-    tax: float | None = None
-
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: list = []
 
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
+async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
     return results
