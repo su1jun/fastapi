681f098ee8d19946772e6e4f7c62a38b0a39bbae
su1jun <qaxsd530@gmail.com>
2025-06-22 23:59:25 +0900

    feat(body) : 모델 중첩, 매개변수에서의 타입 선언

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing import Set, List, Union
+from typing import Set, List, Dict, Union
 from fastapi import FastAPI
 from pydantic import BaseModel, HttpUrl
 
@@ -19,9 +19,29 @@ class Item(BaseModel):
     tags: Set[str] = set()
     images: Union[List[Image], None] = None # second example
 
+# deep recursive model declare
+class Offer(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    items: List[Item]
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
     results = {"item_id": item_id, "item": item}
-    return results
+
+# deep recursive model
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return offer
+
+# list type in argument
+@app.post("/images/multiple/")
+async def create_multiple_images(images: List[Image]):
+    return images
+
+# type type in argument
+@app.post("/index-weights/")
+async def create_index_weights(weights: Dict[int, float]):
+    return weights
 
