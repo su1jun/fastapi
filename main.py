f8eb320c85ca293a92e10557d2fed0df3385099f
su1jun <qaxsd530@gmail.com>
2025-06-24 23:24:28 +0900

    feat(data) : 응답 데이터 예시 속성 추가
    - model-config 속성 이용

--- main.py
+++ main.py
@@ -1,47 +1,29 @@
-from typing import Set, List, Dict, Union
 from fastapi import FastAPI
-from pydantic import BaseModel, HttpUrl
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
-# standard list usage
-# my_list: List[str]
-
-class Image(BaseModel):
-    url: HttpUrl
-    name: str
-
 class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: Set[str] = set()
-    images: Union[List[Image], None] = None # second example
+  name: str
+  description: str | None = None
+  price: float
+  tax: float | None = None
 
-# deep recursive model declare
-class Offer(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    items: List[Item]
+  # reply data example
+  model_config = {
+      "json_schema_extra": {
+          "examples": [
+              {
+                  "name": "Foo",
+                  "description": "A very nice Item",
+                  "price": 35.4,
+                  "tax": 3.2,
+              }
+          ]
+      }
+  }
 
 @app.put("/items/{item_id}")
 async def update_item(item_id: int, item: Item):
-    results = {"item_id": item_id, "item": item}
-
-# deep recursive model
-@app.post("/offers/")
-async def create_offer(offer: Offer):
-    return offer
-
-# list type in argument
-@app.post("/images/multiple/")
-async def create_multiple_images(images: List[Image]):
-    return images
-
-# type type in argument
-@app.post("/index-weights/")
-async def create_index_weights(weights: Dict[int, float]):
-    return weights
-
+  results = {"item_id": item_id, "item": item}
+  return results
