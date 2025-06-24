75e16e6cd010c65b22c404c7b61d340b54572698
su1jun <qaxsd530@gmail.com>
2025-06-24 23:35:37 +0900

    feat(data) : example 여러개 삽입

--- main.py
+++ main.py
@@ -18,11 +18,19 @@ async def update_item(
     Body(
       examples=[
         {
-          "name": "Foo",
-          "description": "A very nice Item",
-          "price": 35.4,
-          "tax": 3.2,
-        }
+            "name": "Foo",
+            "description": "A very nice Item",
+            "price": 35.4,
+            "tax": 3.2,
+        },
+        {
+            "name": "Bar",
+            "price": "35.4",
+        },
+        {
+            "name": "Baz",
+            "price": "thirty five point four",
+        },
       ],
     ),
   ],
