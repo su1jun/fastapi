8069edccbb26654be099306dc815b0aa22c8bd6f
su1jun <qaxsd530@gmail.com>
2025-07-15 23:10:36 +0900

    feat(res) : 동일 응답 데이터 반환
    - 평문 비밀번호 예제(왜??)

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing import Any, List, Union
 
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
 class Item(BaseModel):
@@ -11,11 +11,9 @@ class Item(BaseModel):
     tax: Union[float, None] = None
     tags: List[str] = []
 
-
 @app.post("/items/", response_model=Item)
 async def create_item(item: Item) -> Any:
     return item
-
 
 @app.get("/items/", response_model=List[Item])
 async def read_items() -> Any:
@@ -23,3 +21,15 @@ async def read_items() -> Any:
         {"name": "Portal Gun", "price": 42.0},
         {"name": "Plumbus", "price": 32.0},
     ]
+
+# same input data return
+class UserIn(BaseModel):
+    username: str
+    password: str
+    email: EmailStr
+    full_name: Union[str, None] = None
+
+# Don't do this in production!
+@app.post("/user/")
+async def create_user(user: UserIn) -> UserIn:
+    return user
