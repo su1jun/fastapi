340f5787c53d3b32a9b7fb0b5ec1c8000efdc17d
su1jun <qaxsd530@gmail.com>
2025-07-19 23:33:09 +0900

    feat(res) : model 추가
    - 응답 모델 복습하셈
    - 연관된 모델에 대한 예시(입력, 출력, DB등)

--- main.py
+++ main.py
@@ -1,71 +1,35 @@
-from typing import Any, List, Union
-
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
 app = FastAPI()
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: Union[float, None] = None
-    tags: List[str] = []
 
-@app.post("/items/", response_model=Item)
-async def create_item(item: Item) -> Any:
-    return item
-
-@app.get("/items/", response_model=List[Item])
-async def read_items() -> Any:
-    return [
-        {"name": "Portal Gun", "price": 42.0},
-        {"name": "Plumbus", "price": 32.0},
-    ]
-
-# same input data return  
 class UserIn(BaseModel):
     username: str
     password: str
     email: EmailStr
-    full_name: Union[str, None] = None
+    full_name: str | None = None
 
 class UserOut(BaseModel):
     username: str
     email: EmailStr
-    full_name: Union[str, None] = None
+    full_name: str | None = None
 
-""" # Don't do this in production!
-@app.post("/user/")
-async def create_user(user: UserIn) -> UserIn:
-    return user """
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: EmailStr
+    full_name: str | None = None
+
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
+
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
 
 @app.post("/user/", response_model=UserOut)
-async def create_user(user: UserIn) -> Any:
-    return user
-
-# exclude unset
-class Item(BaseModel):
-    name: str
-    description: Union[str, None] = None
-    price: float
-    tax: float = 10.5
-    tags: List[str] = []
-
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The bartenders", "price": 62, "tax": 20.2},
-    "baz": {"name": "Baz", "description": None, "price": 50.2, "tax": 10.5, "tags": []},
-}
-
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item,
-    response_model_include={"name", "description"},
-)
-async def read_item_name(item_id: str):
-    return items[item_id]
-
-
-@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"})
-async def read_item_public_data(item_id: str):
-    return items[item_id]
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
