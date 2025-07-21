2f5ef8a9ff9cda29419007a77b9ac6b3f8eda45c
su1jun <qaxsd530@gmail.com>
2025-07-21 23:19:38 +0900

    feat(res) : union(anyOf) 사용
    - 여러 타입 중 하나임을 명시
    - 타입 어노테이션 사용은 'some_variable: PlaneItem | CarItem' 처럼

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel, EmailStr
 
@@ -44,3 +45,26 @@ def fake_save_user(user_in: UserIn):
 async def create_user(user_in: UserIn):
     user_saved = fake_save_user(user_in)
     return user_saved
+
+# Union & anyof example
+class BaseItem(BaseModel):
+    description: str
+    type: str
+class CarItem(BaseItem):
+    type: str = "car"
+class PlaneItem(BaseItem):
+    type: str = "plane"
+    size: int
+
+items = {
+    "item1": {"description": "All my friends drive a low rider", "type": "car"},
+    "item2": {
+        "description": "Music is my aeroplane, it's my aeroplane",
+        "type": "plane",
+        "size": 5,
+    },
+}
+
+@app.get("/items/{item_id}", response_model=Union[PlaneItem, CarItem])
+async def read_item(item_id: str):
+    return items[item_id]
